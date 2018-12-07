2ba9ba73439357c217448e343becdd12cf798c8d
Ольга Шишкина <govorkova.t@yandex.ru>
2018-12-07 23:40:13 +0300

    создала класс с данными и с хромосомами

---
+++ GeneticAlgorithm/GeneticAlgorithm/Chromosome.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithm
+{
+    public class Chromosome
+    {
+        public List<int> Genes { get; set; }
+        public int Fitness()
+        {
+            int res = 0;
+            foreach(var i in Genes)
+            {
+                res += i;
+            }
+            return Math.Abs(Data.Value - res);
+        }
+        public Chromosome RandomCrossWith(Chromosome Parent)
+        {
+            Chromosome Child = new Chromosome();
+            Random rnd = new Random();
+            foreach(var g in Genes)
+            {
+                if (rnd.Next(0, 2) == 0)
+                    Child.Genes.Add(g);
+            }
+            foreach (var g in Parent.Genes)
+            {
+                if (rnd.Next(0, 2) == 0)
+                    Child.Genes.Add(g);
+            }
+            return Child;
+        }
+
+        public Chromosome SmartCrossWith(Chromosome Parent)
+        {
+            Chromosome Child = new Chromosome();
+            //здесь будет что то умное
+            return Child;
+        }
+
+        public Chromosome()
+        {
+            Genes = new List<int>();
+        }
+
+        public Chromosome(List<int> genes)
+        {
+            Genes = new List<int>();
+            Random rnd = new Random();
+            foreach (var g in Genes)
+            {
+                if (rnd.Next(0, 2) == 0)
+                    Genes.Add(g);
+            }
+        }
+    }
+}
---
+++ GeneticAlgorithm/GeneticAlgorithm/Data.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithm
+{
+    public static class Data
+    {
+        public static List<int> Set { get; set; }
+        public static int Value { get; set; }
+        public static void Init(List<int> set,int value)
+        {
+            Set = set;
+            Value = value;
+            Set.Sort();
+        }
+    }
+}
